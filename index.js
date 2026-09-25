8d4f9c8c0f143262d451a7f4a8f76e9d9dfbf470
musungu123 <joycemusungu2002@gmail.com>
2026-09-25 08:31:14 +0300

    created five different functions

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+//Function to calculate tax
+function calculateTax(amount) {
+    return amount * 0.1;
+}
+console.log(calculateTax(100));
 
+//Function to convert to upper case
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+console.log(convertToUpperCase('john doe'));
 
+//Function to find maximum
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2);
+}
+console.log(findMaximum(100, 1000));
 
+//Function to is palindrome
+function isPalindrome(word) {
+    let cleaned = word.toLowerCase();
+    let reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
+}
+console.log(isPalindrome('Madam'));
 
+//function to calculate discounted price
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice * (1 - discountPercentage / 100);
+}
+console.log(calculateDiscountedPrice(100, 20));
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice 
